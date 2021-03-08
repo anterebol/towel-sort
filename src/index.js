fb136fc7254f380f90e2940a41b4799dc0efe5e8
unknown <anterebol@gmail.com>
2021-03-08 19:55:57 +0300

    feat: done sumbit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 
-// You should implement your task here.
+module.exports =  function towelSort (arg) {
+ if (arg == undefined) {
+   return [];
+ }
+  if (arg.length < 1) {
+    return [];
+      }
+  let cheng = [];
+  let helper;
+  let d = '';
+  for (let i = 0; i < arg.length; i++) {
+    if ((i + 1)%2 === 0) {
+      helper = arg[i];
+      for ( let k = arg[i].length - 1; k >=0; k--) {
+        cheng.push(helper[k]);
+      }
+      arg[i] = cheng;
+      cheng = [];
+    }
+  }
+  for (let i = 0; i < arg.length; i++){
+    helper = arg[i];
+    for (let k = 0; k < arg[i].length; k++){
+    cheng.push(helper[k]);
+  }}
+    return cheng;
+  }
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+
+
+ 
+  
+  
